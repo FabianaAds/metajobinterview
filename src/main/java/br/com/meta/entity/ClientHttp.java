9986e6b9474c894fb46bd286a87b41de135c19c3
fabiana.silva <fabiana.silva@eemovel.com>
2023-07-14 16:34:37 -0300

    added structure to tests

---
+++ src/main/java/br/com/meta/entity/ClientHttp.java
@@ -0,0 +1,14 @@
+package br.com.meta.entity;
+
+public class ClientHttp {
+
+
+    public ClientHttp(){
+
+
+    }
+    public String Url(){
+         String automationExercise = " https://automationexercise.com/";
+                 return automationExercise;
+    }
+}
